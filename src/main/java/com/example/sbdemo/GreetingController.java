1d1d9896f33fbed4c60089770b52420ae035f0aa
Peter Kipping <pkipping@yahoo.com>
2024-09-23 14:13:53 -0400

    Add home endpoint to GreetingController
    Added home endpoint at /
    Changed GreetingController file
    Added Docker file

--- src/main/java/com/example/sbdemo/GreetingController.java
+++ src/main/java/com/example/sbdemo/GreetingController.java
@@ -16,4 +16,10 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello Test Client";
+	}
+	
 }
